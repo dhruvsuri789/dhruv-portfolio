2be3d82ee31c0e093eb0b88d82d088ee1a4045d0
dhruvsuri789 <dhruvsuri789@gmail.com>
2024-06-02 13:12:28 +0530

    Added pdf file

--- main.js
+++ main.js
@@ -43,3 +43,20 @@ const linkObserver = new IntersectionObserver((entries, _) => {
 }, observerOpts);
 
 allSections.forEach((section) => linkObserver.observe(section));
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => alert("Thank you for your submission"))
+    .catch((error) => alert(error));
+};
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
