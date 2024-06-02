75be5a3d14b7f35278994658e68cdbc035a4e585
dhruvsuri789 <dhruvsuri789@gmail.com>
2024-06-02 14:34:39 +0530

    Added Portfolio image

--- main.js
+++ main.js
@@ -50,12 +50,16 @@ const handleSubmit = (event) => {
   const myForm = event.target;
   const formData = new FormData(myForm);
 
+  console.log(myForm);
+
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => alert("Thank you for your submission"))
+    .then(() => {
+      alert("Thank you for your submission");
+    })
     .catch((error) => alert(error));
 };
 
